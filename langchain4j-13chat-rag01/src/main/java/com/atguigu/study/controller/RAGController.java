003aa20dfd90b24e2be438ad78a971e3e384eb3f
action <xingdong1117@126.com>
2025-11-24 08:19:36 +0800

    feat:langchain4j-13chat-rag01-rag  解决报错

--- langchain4j-13chat-rag01/src/main/java/com/atguigu/study/controller/RAGController.java
+++ langchain4j-13chat-rag01/src/main/java/com/atguigu/study/controller/RAGController.java
@@ -2,13 +2,12 @@ package com.atguigu.study.controller;
 
 import com.atguigu.study.service.ChatAssistant;
 import dev.langchain4j.data.document.Document;
-import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import dev.langchain4j.data.segment.TextSegment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,20 +27,20 @@ public class RAGController
     InMemoryEmbeddingStore<TextSegment> embeddingStore;
 
     @Resource
-    ChatAssistant chatAssistant;
+    private ChatAssistant chatAssistant;
 
     // http://localhost:9013/rag/add
     @GetMapping(value = "/rag/add")
     public String testAdd() throws FileNotFoundException
     {
-        //Document document = FileSystemDocumentLoader.loadDocument("D:\\44\\alibaba-java.docx");
+        //Document document = FileSystemDocumentLoader.loadDocument("/Users/action/Desktop/opt.docx");
 
-        FileInputStream fileInputStream = new FileInputStream("D:\\44\\alibaba-java.docx");
+        FileInputStream fileInputStream = new FileInputStream("/Users/action/Desktop/opt.docx");
         Document document = new ApacheTikaDocumentParser().parse(fileInputStream);
 
         EmbeddingStoreIngestor.ingest(document, embeddingStore);
 
-        String result = chatAssistant.chat("错误码00000和A0001分别是什么");
+        String result = chatAssistant.chat("错误码00000和A0001和B0001分别是什么");
 
         System.out.println(result);
 
